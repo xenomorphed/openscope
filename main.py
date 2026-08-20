619df77bb78a6f951c827ba742c2ebd7c918648a
xenomorphed <olegheksin@gmail.com>
2026-08-20 19:47:00 +0300

    Added basic browser functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-
+import asyncio
+from playwright.async_api import async_playwright
 
 
 print("""
@@ -9,4 +9,21 @@ print("""
  ██║   ██║ ██╔═══╝  ██╔══╝   ██║╚██╗██║ ╚════██║ ██║      ██║   ██║ ██╔═══╝  ██╔══╝
  ╚██████╔╝ ██║      ███████╗ ██║ ╚████║ ███████║ ╚██████╗ ╚██████╔╝ ██║      ███████╗
   ╚═════╝  ╚═╝      ╚══════╝ ╚═╝  ╚═══╝ ╚══════╝  ╚═════╝  ╚═════╝  ╚═╝      ╚══════╝
+
+by xenomorphed
+GitHub: https://github.com/xenomorphed
 """)
+
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+        
+        await page.goto("https://python.org")
+
+        print(page.content)
+
+        await browser.close()
+
+asyncio.run(main())
